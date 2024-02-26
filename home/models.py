30d300c4764c25ef59315cc2038da76bd08f4fb0
Fardin_Zand <officialfardinzand@gmail.com>
2024-02-26 12:48:12 +0330

    fix some Pycharm warning ...

--- home/models.py
+++ home/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='cover')
     # parent = models.ForeignKey('self',related_name='children',on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
+
 
 class MenuItems(models.Model):
     name = models.CharField(max_length=255)
@@ -18,5 +19,6 @@ class MenuItems(models.Model):
     price = models.IntegerField()
     image = models.ImageField(upload_to='cover')
     category = models.ForeignKey(Category,on_delete = models.PROTECT,related_name='MenuItems')
+
     def __str__(self):
         return self.name
