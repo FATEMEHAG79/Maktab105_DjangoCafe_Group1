8e0d309a9672292c2fb17e846d206cc8115c0b8f
Zahra <zahra.dehghaniii98@gmail.com>
2024-02-27 10:36:06 +0330

    Zahra First Commit

--- home/models.py
+++ home/models.py
@@ -43,3 +43,63 @@ class Order(models.Model):
     creat_time = models.DateField(auto_now_add=True, editable=False)
     update_time = models.DateField(auto_now=True, editable=False)
     # one-to-many relation with user
+
+
+
+class User(models.Model):
+    Contact_us=models.ForeignKey(Contact_us, on_delete=models.CASCADE)
+    id=models.IntegerField(Primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number=models.PhoneNumberField((""),unique=True)
+    email=models.EmailField(unique=True)
+    password=models.AutoField(max_length=20,min_length=8 ,unique=True)
+    username=models.CharField(max_length=50 ,min_length=8,unique=True)
+    adress=models.CharField()
+    #Comments=models.ForeignKey(Comments,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+    class Meta():
+        pass
+
+
+
+class Contact_us(models.Model):
+    id=models.IntegerField(max_length=20,unique=True)
+    full_name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
+    phone_number=models.PhoneNumberField((""),unique=True)
+    #Comments=models.ForeignKey(Comments,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        pass
+
+
+
+class Comments(models.Model):
+    User=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=30)
+    text=models.CharField(max_length=150)
+    date_text=models.DateTimeField()
+    MenuItem=models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    class Meta():
+        pass
+
+
+
+
+
+    
+
+
