a0d611c4553ffa1b3993a27681ad376b80763824
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-02-29 15:16:20 +0330

    admin regester add models

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Comments)
+admin.site.register(Table)
+admin.site.register(MenuItems)
+
