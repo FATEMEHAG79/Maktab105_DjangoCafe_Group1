e362fd4f8410a71d105f4e87d4b2f940e4187b2c
alibaghani <baghaniali2006@gmail.com>
2024-03-03 19:39:02 +0330

    Profile view for profile was added

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, DetailView, UpdateView
+from .models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='login')
+class ProfileView(DetailView):
+    model = User
+    template_name = 'profile.html'
+    context_object_name = 'users'
+
+
