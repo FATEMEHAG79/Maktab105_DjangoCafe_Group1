e1ed7793ef6dfb6223fe5ac85b142695d4e4b8aa
Fardin_Zand <officialfardinzand@gmail.com>
2024-03-04 12:28:29 +0330

    Add MenuItems, Order, and OrderItem views - Added MenuItemsListView, OrderListView, and OrderItemCreateView to handle the views for listing menu items, listing active orders, and creating order items respectively. These views use Django's generic class-based views for simplicity and efficiency.

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, DetailView, UpdateView
-from .models import User
+from django.views.generic import TemplateView, DetailView, UpdateView, ListView, CreateView
+from .models import User, MenuItems, Order, OrderItem
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+
 
 @login_required(login_url='login')
 class ProfileView(DetailView):
@@ -11,8 +12,35 @@ class ProfileView(DetailView):
     template_name = 'profile.html'
     context_object_name = 'users'
 
+
 class Changeinformation(UpdateView):
     model = User
     fields = ['username', 'phone_number']
     template_name = 'changeinformation.html'
     success_url = reverse_lazy('success-url')
+
+
+class MenuItemsView(ListView):
+    model = MenuItems
+    template_name = 'Order_list.html'
+    context_object_name = 'menu_items'
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return Order.objects.filter(completed=False)
+
+
+class OrderItemCreateView(CreateView):
+    model = OrderItem
+    template_name = 'create_order_item.html'
+    fields = ['menuitem', 'quantity']
+    success_url = reverse_lazy('order_list')
+
+    def form_valid(self, form):
+        form.instance.order = Order.objects.get(id=self.kwargs['order_id'])
+        return super().form_valid(form)
