159c371784a1b90d3cf5adbb1d3f5d38f6ce0b08
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-03-04 21:53:00 +0330

    eddit models user and add myusermanager

--- coffeeshop/models.py
+++ coffeeshop/models.py
@@ -1,28 +1,51 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin,BaseUserManager
 from django.db import models
 
 
-# Create your models here.PermissionsMixin
+class MyUsertManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
 
-# Create your models here.
-class User(models.Model):
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=20, unique=True)
-    username = models.CharField(max_length=50, unique=True)
     address = models.TextField()
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = MyAccountManager()
+
     def __str__(self):
-        return (
-            f"ID: {self.id}, "
-            f"Name: {self.first_name} {self.last_name}, "
-            f"Phone: {self.phone_number}, "
-            f"Email: {self.email}, "
-            f"Username: {self.username}, "
-            f"Address: {self.address}"
-        )
+        return self.username
 
 
 class Category(models.Model):
