6af065a363292322ac72ade66bf353e2d9cfe172
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-03-07 14:55:34 +0330

    start app account and add model user

---
+++ account/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin,BaseUserManager
+from django.db import models
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=11, unique=True)
+    address = models.TextField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.username
