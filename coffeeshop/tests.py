5168affbe5cb76098a84b0f9b83f1c0bc2ca75ff
alibaghani <baghaniali2006@gmail.com>
2024-03-08 11:12:08 +0330

    needed moduls were imported

--- coffeeshop/tests.py
+++ coffeeshop/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase
+from django.core.files import File
+from django.urls import reverse
 
-# Create your tests here.
+
