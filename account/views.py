f24743537896f684667faee3796e60d6456b7c61
alibaghani <baghaniali2006@gmail.com>
2024-03-08 16:05:59 +0330

    logout view was updated

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView as _LoginView, RedirectURLMixin , LogoutView as _LogoutView
+from django.contrib.auth.views import LoginView as _LoginView, RedirectURLMixin, LogoutView as _LogoutView
 from django.urls import reverse_lazy
 from django.views import generic, View
 from django.forms import Form
@@ -7,11 +8,11 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from account.models import *
 from django.contrib import messages
 from .models import User
-
+from django.contrib.auth import login, authenticate, logout
 
 
 # Create your views here.
-#html address must change
+# html address must change
 
 class LoginView(_LoginView):
     redirect_authenticated_user = True
@@ -22,31 +23,28 @@ class LoginView(_LoginView):
     def form_valid(self, form):
         result = super().form_valid(form)
         return result
-    
-#html address must change
+
+
+# html address must change
 class RegisterView(generic.CreateView):
-    template_name = "auth/register.html"
+    template_name = "register.html"
     model = User
-    fields = ['first_name', 'last_name', 'phone_number', 'password','email','address']
-    success_url = "/login"
+    fields = ['username','first_name', 'last_name', 'phone_number', 'password', 'email', 'address']
+    success_url = reverse_lazy('coffeeshop')
 
     def form_valid(self, form):
         user = form.save(commit=False)
         user.set_password(form.cleaned_data['password'])
         user.save()
 
-        return super().form_valid(form)    
+        return super().form_valid(form)
 
 
-class LogoutView(_LogoutView):
-    # redirect_authenticated_user=False
-    next_page = reverse_lazy("contact")
-    # template_name="logout.html"
-    # url = "/"
+class LogoutView(generic.RedirectView):
+    url = "/"
 
     def get(self, request, *args, **kwargs):
         logout(request)
 
         messages.info(self.request, "Bye Bye 👋🏻")
         return super().get(self.request, *args, **kwargs)
-
