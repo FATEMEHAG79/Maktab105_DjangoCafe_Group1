a36044e06bc23243b5fdd658542b344577cd235f
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-03-08 10:38:43 +0330

    edit user model

--- account/models.py
+++ account/models.py
@@ -31,20 +31,10 @@ class MyUserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=11, unique=True)
     address = models.TextField()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
     objects = MyUserManager()
-
     def __str__(self):
         return self.username
