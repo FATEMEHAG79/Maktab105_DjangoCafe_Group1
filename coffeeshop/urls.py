a8bb6462e6dedf9a81c29928907c3a1141e54759
alibaghani <baghaniali2006@gmail.com>
2024-03-10 02:38:04 +0330

    I added the home page's link and about page's link

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from .views import AboutView
+
+
+urlpatterns = [
+    path('',TemplateView.as_view(template_name='home.html'), name='coffeeshop'),
+    path('about/',AboutView.as_view(), name='about')
+]
