4a53cf532253604d291edd0ece66be265ab52a70
alibaghani <baghaniali2006@gmail.com>
2024-03-10 03:08:54 +0330

    change information url was added

--- account/url.py
+++ account/url.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import LoginView,LogoutView,RegisterView
 from coffeeshop.views import ProfileView
+from coffeeshop.views import ChangeinformationView
 
 urlpatterns = [
     path('login/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
     path('signup/',RegisterView.as_view(),name="signup"),
-    path('profile/<int:pk>/', ProfileView.as_view(), name="profile")
+    path('profile/<int:pk>/', ProfileView.as_view(), name="profile"),
+    path('changeinformations/<int:pk>/', ChangeinformationView.as_view(), name="change")
 ]
