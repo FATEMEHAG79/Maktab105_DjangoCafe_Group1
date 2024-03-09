06948cec07dc47db8316d7ba6fa0afde60d0d168
alibaghani <baghaniali2006@gmail.com>
2024-03-10 01:37:29 +0330

    some changes in urls

--- account/url.py
+++ account/url.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import LoginView,LogoutView,RegisterView
+from coffeeshop.views import ProfileView
 
 urlpatterns = [
     path('login/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
-    path('signup/',RegisterView.as_view(),name="signup")
+    path('signup/',RegisterView.as_view(),name="signup"),
+    path('profile/<int:pk>/', ProfileView.as_view(), name="profile")
 ]
