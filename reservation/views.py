efa979e7212263038cfed79799c56e1fbdceb4fa
Zahra <zahra.dehghaniii98@gmail.com>
2024-03-14 03:31:09 +0330

    changes in resevationapp

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . forms import ReservationForm
 
 # Create your views here.
+def reserve(request):
+    reserve_form=ReservationForm()
+    if request.method=="post":
+        reserve_form=ReservationForm(request.POST)
+        if reserve_form.is_valid():
+            reserve_form.save()
+    else:
+        reserve_form=ReservationForm()
+
+    
+    
+    context={
+        "form":reserve_form,
+    }
+    return render(request,'templates/reservation.html',context)
+
+
