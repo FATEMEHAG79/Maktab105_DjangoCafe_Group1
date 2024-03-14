27422b72c909f2d2828480f5151e37f487851081
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-03-14 07:59:38 +0330

    add view add and update and checkout to cart

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from . import views
-from .views import AboutView
+from .views import AboutView,add_to_cart,checkout,update_cart
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='coffeeshop'),
     path('about/', AboutView.as_view(), name='about'),
     path('itemdetail/<int:pk>/', views.ItemDetailView.as_view(), name='item_detail'),
+    path('add-to-cart/', add_to_cart, name='add-to-cart'),
+    path('checkout/', checkout, name="checkout"),
+    path('update-cart/', update_cart, name='update-cart')
 ]
--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import get_object_or_404, render
+import json
+
+from django.shortcuts import get_object_or_404, render,redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView, DetailView
-
-from .models import MenuItems, Order, OrderItem
+from django.http import JsonResponse
+from .models import MenuItems, Order, OrderItem, Cart
 
 
 # Create your views here.
@@ -56,3 +58,63 @@ class ItemDetailView(DetailView):
     def get(self,request, pk):
         item=get_object_or_404(MenuItems, pk=pk)
         return render(request,"ItemDetailView.html",{"item":item})
+
+
+def add_to_cart(request):
+    current_user = request.user
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            product_id = int(request.POST.get('product_id'))
+            # print('injaaaaa hastamm   product_id', product_id)
+            product_check = MenuItems.objects.get(id=product_id)
+            if product_check:
+                if Cart.objects.filter(user=current_user, product_id=product_id):
+                    return JsonResponse({'status': 'Product is Already in Cart'})
+                else:
+                    product_qyt = 1
+                    Cart.objects.create(user=current_user, product_id=product_id, quantity=product_qyt)
+                    return JsonResponse({'status': 'Product added successfuly'})
+            else:
+                return JsonResponse({'status': 'No such Product found'})
+        else:
+            return JsonResponse({'status': "Login to continue"})
+    return redirect('/')
+def checkout(request):
+    context = {}
+    cart_items = Cart.objects.filter(user=request.user)
+
+    context['cart_items'] = cart_items
+    context['cart_total'] = cart_items.count()
+
+    cart_total = 0
+    total_price = 0
+    if cart_items:
+        for item in cart_items:
+            total_price += (item.product.price) * item.quantity
+            cart_total += item.quantity
+
+        context['total_price'] = total_price
+        context['cart_total'] = cart_total
+
+    return render(request, 'checkout.html', context)
+
+
+def update_cart(requset):
+    data = json.loads(requset.body)
+    prod_id = data['productId']
+    action = data['action']
+
+    cart_item = Cart.objects.filter(user=requset.user, product_id=prod_id)[0]
+
+    if cart_item:
+        if action == 'add':
+            cart_item.quantity += 1
+        elif action == 'remove':
+            cart_item.quantity -= 1
+
+        cart_item.save()
+
+        if cart_item.quantity == 0:
+            cart_item.delete()
+
+    return JsonResponse({'status': "Update Successfully"})
