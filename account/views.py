08a1728f36121ea8c4dbfd528b2259a519c6afb9
Etthadie <m2e.1384@gmail.com>
2024-03-14 05:33:30 +0330

    change and remove some imports

--- account/views.py
+++ account/views.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView as _LoginView, RedirectURLMixin, LogoutView as _LogoutView
-from django.urls import reverse_lazy
-from django.views import generic, View
-from django.forms import Form
-from django.contrib.auth import get_user_model, authenticate, login, logout
-from account.models import *
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView as _LoginView, PasswordResetConfirmView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import UpdateView, DetailView
+
+from .forms import CustomPasswordChangeForm
 from .models import User
-from django.contrib.auth import login, authenticate, logout
 
 
 # Create your views here.
@@ -29,7 +29,7 @@ class LoginView(_LoginView):
 class RegisterView(generic.CreateView):
     template_name = "register.html"
     model = User
-    fields = ['username','first_name', 'last_name', 'phone_number', 'password', 'email', 'address']
+    fields = ['username', 'first_name', 'last_name', 'phone_number', 'password', 'email', 'address']
     success_url = reverse_lazy('coffeeshop')
 
     def form_valid(self, form):
